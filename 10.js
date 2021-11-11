00e02ded550934d79e6ccc2ac6e0c54954aef254
anupam <annupam2003@gmail.com>
2021-11-11 20:02:06 +0530

    10 Template String
    Templage String

---
+++ 10.js
@@ -0,0 +1,10 @@
+//Template string
+
+let age=18;
+let name='baba'
+
+let aboutme = 'my name is '+name+ ' and my age is '+age;
+console.log(aboutme);
+
+aboutme = `my name is ${name} and my age is ${age}`;
+console.log(aboutme);
