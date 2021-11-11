b7aa85131c6cb1b354e85e24d1523c8cc6ff1afc
anupam <annupam2003@gmail.com>
2021-11-11 19:50:18 +0530

    09 Concatenation string
    Concatenation two string and also string variable convert perform sum operation

---
+++ 09.js
@@ -0,0 +1,16 @@
+// Concatenation two strings
+
+let var1='baba';
+let var2='Kumar';
+
+let var3 = var1+' '+var2;
+console.log(var3);
+
+var1='10';
+var2='20';
+
+var3 = var1+' '+var2;
+console.log(var3);
+
+var3 = +var1+ +var2;
+console.log(var3);
