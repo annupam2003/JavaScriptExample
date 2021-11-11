e481cba1282ccd9082df54d5388d954e7d131213
anupam <annupam2003@gmail.com>
2021-11-11 21:34:51 +0530

    11 Null Vs Undefined
    Null Vs Undefined

---
+++ 11.js
@@ -0,0 +1,16 @@
+let name;
+
+console.log(name);
+console.log(typeof name);
+
+name='kumar';
+console.log(name);
+
+
+let myvar=null;
+
+console.log(myvar);
+console.log(typeof myvar);
+
+myvar=10;
+console.log(myvar);
