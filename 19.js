0b8e37a4d6a4b3ee79c3fbc72633131a77b2c5f0
anupam <annupam2003@gmail.com>
2021-11-14 18:39:48 +0530

    19 switch case
    Switch Case

---
+++ 19.js
@@ -0,0 +1,29 @@
+//switch statment
+
+let day = 1
+switch(day){
+    case 0:
+        console.log('Day is Sunday');
+        break;
+    case 1:
+        console.log('Day is Monday');
+        break;
+    case 2:
+        console.log('Day is Tuesday');
+        break;
+    case 3:
+        console.log('Day is Wednesday');
+        break;
+    case 4:
+        console.log('Day is Thrusday');
+        break;
+    case 5:
+        console.log('Day is Friday');
+        break;
+    case 6:
+        console.log('Day is Saturday');
+        break;
+    default:
+            console.log('Day is Invalid');
+
+}
