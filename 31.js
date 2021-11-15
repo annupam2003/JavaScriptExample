32e99d163818bf5e3cf10fa831f020915027a082
anupam <annupam2003@gmail.com>
2021-11-15 22:26:20 +0530

    31 Array with const
    Array with const

---
+++ 31.js
@@ -0,0 +1,12 @@
+const pi =3.14
+console.log(pi);
+
+// pi=33.7;
+
+const fruits =['apple','mango','grapes'];
+fruits.push('banana');
+//Array is reference type momey is not reasign so that it's possible
+console.log(fruits);
+
+//Array is reference type momey is not reasign so that it's not possible
+fruits=[];
