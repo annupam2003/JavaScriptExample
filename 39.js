2a662be678e9e182747106875f9c5e6387b6f8c0
anupam <annupam2003@gmail.com>
2021-11-20 00:04:57 +0530

    39 computed properties
    computed properties

---
+++ 39.js
@@ -0,0 +1,12 @@
+const key1='mykey1';
+const key2='mykey2';
+const val1='myval1';
+const val2='myval2';
+
+//computed property
+const obj ={
+    [key1]:val1,
+    [key2]:val2
+}
+
+console.log(obj);
