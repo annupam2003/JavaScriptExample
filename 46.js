86e175c4e9aff751d814f04ae87b3e7a4697076d
anupam <annupam2003@gmail.com>
2021-11-22 23:37:48 +0530

    46 Arrow Function
    Lamda Expression

---
+++ 46.js
@@ -0,0 +1,14 @@
+// arrow function
+
+function func()
+{
+    console.log('hello javascript');
+}
+
+func();
+
+const func1 = () => console.log('hello javascript');
+func1();
+
+const addition = (num1,num2) =>  num1+num2;
+console.log(addition(2,4));      //agrument
