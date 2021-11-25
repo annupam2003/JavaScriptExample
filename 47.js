2a4071fedf116d8ec7f5a371532cf7a7a419f85f
anupam <annupam2003@gmail.com>
2021-11-25 20:18:23 +0530

    47 Hositing Introduction
    Hositing Introduction

---
+++ 47.js
@@ -0,0 +1,16 @@
+// Hoisting 
+
+func();
+
+function func(){
+    console.log("Hosting Behaviour");
+}
+
+console.log(num);
+var num='no error found';
+
+console.log(num2)
+let num2='error found';
+
+console.log(num3);
+const num3='errof found';
