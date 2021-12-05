df9dd99338ffcf95170e5e3a1a2d3cd30613045e
anupam <annupam2003@gmail.com>
2021-12-05 21:56:55 +0530

    64 fill method
    fill method

---
+++ 64.js
@@ -0,0 +1,8 @@
+//fill method
+// value, start(index), end(length)
+
+const myArray = new Array(10).fill(22);
+console.log(myArray);
+
+const myArray2 = [1,2,3,4,5,6,7,8,9];
+console.log(myArray2.fill(54,3,6));
