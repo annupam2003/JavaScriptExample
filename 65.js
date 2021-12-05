36449fa683e2e170e098b5782bfdc3ba0e71f309
anupam <annupam2003@gmail.com>
2021-12-05 22:32:28 +0530

    65 splice method
    splice method

---
+++ 65.js
@@ -0,0 +1,14 @@
+//splice method
+// start, delete(index), insert(length)
+//mutable
+
+
+
+const myArray = [1,2,3,4,5,6,7,8,9];
+
+//delete
+console.log(myArray.splice(1,2));
+
+const myArray2 = [1,2,3,4,5,6,7,8,9];
+
+console.log(myArray2.splice(2,5));
